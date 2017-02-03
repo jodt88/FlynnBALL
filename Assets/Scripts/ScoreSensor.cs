b1568bbcd1829048af093ea77339c87dc761324c
jodt88 <jtoms5@lsu.edu>
2017-02-02 21:32:58 -0600

    Configure score counter.
    Added fonts for score counter, added "antagonist" ball asset, wrote a
    script for the score counter, tested score counter.

---
+++ Assets/Scripts/ScoreSensor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreSensor : MonoBehaviour
+{
+    private int score = 0;
+
+    public GameObject scoreText;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.name == "ball")
+        {
+            score = score + 100;
+        }
+        else
+        {
+            score = score - 100;
+        }
+    }
+
+    void Update()
+    {
+        scoreText.GetComponent<TextMesh>().text = "Score: " + score;
+    } 
+}
